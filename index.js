5f7927ba3b247d79da04e75dcbc1160e964b5e67
Raju04p <154728176+Raju04p@users.noreply.github.com>
2024-08-16 15:39:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,41 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/api/:date?", function (req, res) {
+    let  date =  req.params.date;
+    let unixDate;
+    let dateObj;
+    let utcDate;
+
+  // test wheather the input date is a number
+  let isUnix = /^\d+$/.test(date);
+
+  // if no date specified current date
+  if(!date){
+    dateObj = new Date();
+  }
+  // if our input is a unix string we set the unixDate as the input
+  else if( date && isUnix ){
+    unixDate = parseInt(date);
+    dateObj = new Date(unixDate);
+  }
+    // date not is not unix time stamp
+  else if(date && !isUnix){
+    dateObj = new Date(date);
+  }
+  if (dateObj.toString() === "Invalid Date"){
+    res.json({error: "Invalid Date"});
+    return;
+  }
+  unixDate = dateObj.getTime();
+  utcDate = dateObj.toUTCString();
+    res.json({
+      unix: unixDate,
+      utc: utcDate
+    });
 });
+
+
 
 
 // your first API endpoint... 
